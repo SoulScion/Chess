02d886d5f77871890f047687a8cc6f98f2f61c95
SoulScion <roxy007x3@gmail.com>
2024-04-10 02:57:00 -0600

    Fixing up stuff for Phase 6

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import ui.ClientRepl;
 
 public class Main {
     public static void main(String[] args) {
+        var serverLink = "http://localhost:8080";
+        if (args.length == 1) {
+            serverLink = args[0];
+        }
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        new ClientRepl(serverLink).run();
     }
 }
